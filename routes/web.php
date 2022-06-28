8e7b378190c8c87241af56c193e35a8408de737a
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-06-28 18:01:01 -0300

    Learn to create routes in Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get('courses', [CourseController::class, 'index']);
+
+Route::get('courses/create', [CourseController::class, 'create']);
+
+Route::get('courses/{course}', [CourseController::class, 'show']);
