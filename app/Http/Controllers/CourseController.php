cd0b3c9a35f35d07f528e983669f4e846643aadc
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-11 17:19:43 -0300

    Use 'Course' model to retrieve information from database

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
 
 class CourseController extends Controller
 {
     public function index() {
-        return view('courses.index');
+        $courses = Course::simplePaginate(5);
+        return view('courses.index', ['courses' => $courses]);
     }
 
     public function create() {
         return view('courses.create');
     }
 
-    public function show($course) {
+    public function show($id) {
+        $course = Course::find($id);
         // Use compact() when there are many variables besides $course
         return view('courses.show', ['course' => $course]);
     }
