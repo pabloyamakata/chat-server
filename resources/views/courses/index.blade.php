45d938292a183ae1b79c6323d79b0cbbed09e971
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-11 17:24:54 -0300

    List all courses and add pagination

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -4,4 +4,13 @@
 
 @section('content')
     <h1>Welcome to my courses!</h1>
+    <a href={{ route('courses.create'); }}>Create Course</a>
+    <ul>
+        @foreach ($courses as $course)
+            <li>
+                <a href={{ route('courses.show', $course->id); }}>{{ $course->name }}</a>
+            </li>
+        @endforeach
+    </ul>
+    {{ $courses->links() }}
 @endsection
