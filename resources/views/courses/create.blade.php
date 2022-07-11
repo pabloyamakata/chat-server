fa6794c31407966c644f797e05c0040b4774ea90
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-11 19:33:53 -0300

    Create form to insert new courses into the database

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -4,4 +4,26 @@
 
 @section('content')
     <h1>On this page you can create new courses!</h1>
+    <form action={{ route('courses.store'); }} method="POST">
+        @csrf {{-- Sends a token to prevent csrf --}}
+        <label>
+            Name:
+            <br>
+            <input type="text" name="name">
+        </label>
+        <br><br>
+        <label>
+            Description:
+            <br>
+            <textarea name="description" rows="5"></textarea>
+        </label>
+        <br><br>
+        <label>
+            Category:
+            <br>
+            <input type="text" name="category">
+        </label>
+        <br><br>
+        <button type="submit">Submit</button>
+    </form>
 @endsection
