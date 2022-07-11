0a561552cc07f42ce83c23ef099970eb45e359ff
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-11 19:48:20 -0300

    Create new method store to insert new courses into the database

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -8,12 +8,21 @@ use App\Models\Course;
 class CourseController extends Controller
 {
     public function index() {
-        $courses = Course::simplePaginate(5);
+        $courses = Course::orderby('id', 'desc')->simplePaginate(5);
         return view('courses.index', ['courses' => $courses]);
     }
 
     public function create() {
         return view('courses.create');
+    }
+
+    public function store(Request $request) {
+        $course = new Course();
+        $course->name = $request->name;
+        $course->description = $request->description;
+        $course->category = $request->category;
+        $course->save();
+        return redirect()->route('courses.show', $course->id);
     }
 
     public function show($id) {
