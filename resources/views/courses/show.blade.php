9de6259d1a7c65a78b43287a49e2ac93d9f15db3
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-11 17:29:42 -0300

    Show data about a specific course

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.template')
 
-@section('title', $course . ' Course')
+@section('title', $course->name . ' Course')
 
 @section('content')
-    <h1>This page will show a specific course like {{ $course }}!</h1>
+    <h1>This page will show a specific course like {{ $course->name }}!</h1>
+    <a href={{ route('courses.index'); }}>Go to courses</a>
+    <p><strong>Category: </strong>{{ $course->category }}</p>
+    <p>{{ $course->description }}</p>
 @endsection
