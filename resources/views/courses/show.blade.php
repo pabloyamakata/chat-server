53285f34b090d7ec395054c9e12d3da5f1df6269
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-13 20:24:13 -0300

    Add link that redirects to another page where user will be able to edit course

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -5,6 +5,8 @@
 @section('content')
     <h1>This page will show a specific course like {{ $course->name }}!</h1>
     <a href={{ route('courses.index'); }}>Go to courses</a>
+    <br>
+    <a href={{ route('courses.edit', $course->id); }}>Edit Course</a>
     <p><strong>Category: </strong>{{ $course->category }}</p>
     <p>{{ $course->description }}</p>
 @endsection
