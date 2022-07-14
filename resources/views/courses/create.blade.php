24a46d50e0589de4784dd0b2fce73f93750cd95f
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-13 22:07:55 -0300

    feat: Display error messages for every input and avoid resetting what the user has typed after the error has occured

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -9,20 +9,35 @@
         <label>
             Name:
             <br>
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{ old('name') }}">
         </label>
+        @error('name')
+            <br>
+            <small>*{{ $message }}</small>
+            <br>
+        @enderror
         <br><br>
         <label>
             Description:
             <br>
-            <textarea name="description" rows="5"></textarea>
+            <textarea name="description" rows="5">{{ old('description') }}</textarea>
         </label>
+        @error('description')
+            <br>
+            <small>*{{ $message }}</small>
+            <br>
+        @enderror
         <br><br>
         <label>
             Category:
             <br>
-            <input type="text" name="category">
+            <input type="text" name="category" value="{{ old('category') }}">
         </label>
+        @error('category')
+            <br>
+            <small>*{{ $message }}</small>
+            <br>
+        @enderror
         <br><br>
         <button type="submit">Submit</button>
     </form>
