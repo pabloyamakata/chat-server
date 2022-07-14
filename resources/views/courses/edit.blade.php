d634a6c74a1bac8a88674c9bbcb6973bdbf87f74
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-14 17:49:11 -0300

    feat: Display error messages and remember any changes made by the user

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -5,26 +5,50 @@
 @section('content')
     <h1>On this page you can edit a course!</h1>
     <form action={{ route('courses.update', $course->id); }} method="POST">
+        
         @csrf {{-- Sends a token to prevent csrf --}}
+        
         @method('PUT')
+        
         <label>
             Name:
             <br>
-            <input type="text" name="name" value="{{ $course->name }}">
+            <input type="text" name="name" value="{{ old('name', $course->name) }}">
         </label>
+
+        @error('name')
+            <br>
+            <small>*{{ $message }}</small>
+            <br>
+        @enderror
         <br><br>
+        
         <label>
             Description:
             <br>
-            <textarea name="description" rows="5">{{ $course->description }}</textarea>
+            <textarea name="description" rows="5">{{ old('description', $course->description) }}</textarea>
         </label>
+
+        @error('description')
+            <br>
+            <small>*{{ $message }}</small>
+            <br>
+        @enderror
         <br><br>
+        
         <label>
             Category:
             <br>
-            <input type="text" name="category" value="{{ $course->category }}">
+            <input type="text" name="category" value="{{ old('category', $course->category) }}">
         </label>
+
+        @error('category')
+            <br>
+            <small>*{{ $message }}</small>
+            <br>
+        @enderror
         <br><br>
+
         <button type="submit">Submit</button>
     </form>
 @endsection
